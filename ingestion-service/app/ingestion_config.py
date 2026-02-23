109a7cda2c9f40be45189ba05fb546128d2c4964
Moshe <moshe@example.com>
2026-02-23 15:29:22 +0200

    fix: config file

--- ingestion-service/app/ingestion_config.py
+++ ingestion-service/app/ingestion_config.py
@@ -1,8 +1,16 @@
 import os 
+from pathlib import Path
 
 class IngestionConfig():
     def __init__(self):
-        self.kafka_host = os.getenv("KAFKA_HOST", "localhost")
-        self.kafka_port = int(os.getenv("KAFKA_PORT", "9092"))
-        self.kafka_config = {"bootstrap.servers" : f"{self.kafka_host}:{self.kafka_port}"}
-        self.source_route = "data\messaging_images.zip"
+        kafka_host = os.getenv("KAFKA_HOST", "localhost")
+        kafka_port = os.getenv("KAFKA_PORT", "9092")
+        self.kafka_bootstrap_servers = f"{kafka_host}:{kafka_port}"
+        self.kafka_config = {"bootstrap.servers":self.kafka_bootstrap_servers} 
+        self.source_route = Path("data") / "messaging_images"
+        self.mongo_loader_url = os.getenv("MONGO_LOADER_URL","http://localhost:27017")
+    
+    def validate(self):
+        if not self.kafka_config or not self.source_route or not self.mongo_loader_url:
+            raise "configuration messing"
+        
