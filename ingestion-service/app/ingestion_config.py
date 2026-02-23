0081ff5eafa939311efe96195736ace2a2810783
Moshe <moshe@example.com>
2026-02-23 17:41:57 +0200

    feat: add more  fields

--- ingestion-service/app/ingestion_config.py
+++ ingestion-service/app/ingestion_config.py
@@ -5,9 +5,12 @@ class IngestionConfig():
     def __init__(self):
         kafka_host = os.getenv("KAFKA_HOST", "localhost")
         kafka_port = os.getenv("KAFKA_PORT", "9092")
-        self.kafka_bootstrap_servers = f"{kafka_host}:{kafka_port}"
-        self.kafka_config = {"bootstrap.servers":self.kafka_bootstrap_servers} 
-        self.source_route = Path("data") / "messaging_images"
+        kafka_bootstrap_servers = f"{kafka_host}:{kafka_port}"
+
+        self.log_level = os.getenv("LOG_LEVEL","INFO")
+        self.service_name = os.getenv("SERVICE_NAME","ingestion_service")
+        self.kafka_config = {"bootstrap.servers" : kafka_bootstrap_servers} 
+        self.kafka_topic_name = os.getenv("KAFKA_TOPIC_NAME","RAW")
         self.mongo_loader_url = os.getenv("MONGO_LOADER_URL","http://localhost:27017")
     
     def validate(self):
