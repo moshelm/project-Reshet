669d889f96c6002a22eb3661e69f4b30ca87ebbd
Moshe <moshe@example.com>
2026-02-23 12:05:28 +0200

    fix: adding logger and add function

--- ingestion-service/app/ocr_engine.py
+++ ingestion-service/app/ocr_engine.py
@@ -1,4 +1,19 @@
+import logging 
+import pytesseract as pt
+from PIL import Image
+
+
 class OCREngine():
     def __init__(self,image):
-        self.image = image
-
+        self.logger = logging.getLogger(__name__)
+    
+    def extract_text(self,image_path:str):
+        try:
+            img = Image.open(image_path)
+        except FileNotFoundError as e:
+            self.logger.error("image not found" ,exc_info=e)
+        try:
+            text = pt.image_to_string(img)
+            return text 
+        except Exception as e:
+            self.logger.error("failed convert image to string", exc_info=e)
