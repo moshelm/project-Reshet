7d324b472c19f539e533534a99cb657d91082d71
Moshe <moshe@example.com>
2026-02-24 12:40:22 +0200

    fix: change logic that send the hole file in binary mode

--- ingestion-service/app/mongo_loader_client.py
+++ ingestion-service/app/mongo_loader_client.py
@@ -9,9 +9,14 @@ class MongoLoaderClient():
     
     def send(self,file_path:str, image_id:str):
         try:
-            response = requests.post(self.mongo_loader_url,data=file_path, params= image_id)
-            self.logger.info("sending to mongo success")
+            with open(file_path,"br") as file:
+                files = {"file":file}
+                response = requests.post(self.mongo_loader_url,files=files)
+                self.logger.info("sending to mongo success")
             return response
+        except FileNotFoundError as e:
+            self.logger.error(f"file not found to send {e}",exc_info=True)
+            return None
         except RequestException as e:
-            self.logger.error(f"failed to send to mongo loader {e}")
+            self.logger.error(f"failed to send to mongo loader {e}",exc_info=True)
             return None
