4aca0184898f7a1dd6c6a70969b4a86b9d49b9df
Moshe <moshe@example.com>
2026-02-24 15:52:30 +0200

    fix: mongo_uri using mongodb not http

--- mongo_loader/app/service_config.py
+++ mongo_loader/app/service_config.py
@@ -8,9 +8,10 @@ class MongoConfig():
         mongo_port = os.getenv("MONGO_PORT","27017")
         
         self.mongo_database = os.getenv("MONGO_DATABASE","db_reshet")
-        self.mongo_uri = f"http://{mongo_host}:{mongo_port}"
+        self.mongo_uri = f"mongodb://{mongo_host}:{mongo_port}"
         self.log_level = os.getenv("LOG_LEVEL","INFO")
         self.service_name = os.getenv("SERVICE_NAME","mongo")
+        
     def validate(self):
         if not self.mongo_uri or not self.log_level or not self.service_name:
             raise "configuration is missing"
