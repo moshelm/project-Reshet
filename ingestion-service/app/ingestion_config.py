9e19b4d13b76d0201af31ac6bc05ecc881c71579
Moshe <moshe@example.com>
2026-02-24 09:42:23 +0200

    fix: mongo_loader_url fix routing to /data in root dir

--- ingestion-service/app/ingestion_config.py
+++ ingestion-service/app/ingestion_config.py
@@ -13,7 +13,7 @@ class IngestionConfig():
         self.kafka_topic_name = os.getenv("KAFKA_TOPIC_NAME","RAW")
         self.mongo_loader_url = os.getenv("MONGO_LOADER_URL","http://localhost:27017")
 
-        self.data_files_route = Path("data") / "messaging_images" / "tweet_images"
+        self.data_files_route = Path("/data") / "messaging_images" / "tweet_images"
     
     def validate(self):
         if not self.kafka_config or not self.data_files_route or not self.mongo_loader_url:
