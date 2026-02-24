f100c049a5d7a345eb695c3d7f57a6521e30cad8
Moshe <moshe@example.com>
2026-02-24 15:51:59 +0200

    feat: create orchestrator

---
+++ mongo_loader/app/mongo_orchestrator.py
@@ -0,0 +1,27 @@
+from logging import Logger
+from mongo_loader.app.mongo_manager import MongoManager
+from fastapi import HTTPException, File, UploadFile, Form
+
+class MongoOrchestrator():
+    def __init__(self,storage: MongoManager, logger :Logger):
+        self.logger = logger
+        self.storage = storage
+
+    def upload_file(self,request: UploadFile = File(...), image_id: str = Form(...)):
+        self.logger.info("upload file")
+        result = self.storage.save(request, image_id)
+        return result
+
+    def run(self,request: UploadFile = File(...), image_id: str = Form(...)):
+        self.logger.info("start running")
+        try: 
+            result = self.upload_file(request,image_id)
+            if result is None:
+                raise Exception 
+            self.logger.info("finish insertion to mongo")
+            return result
+        except Exception as e:
+            self.logger.error(f"failed insert to mongo. {e}",exc_info=True)
+            raise HTTPException(f"server failed. {e}", status_code=400)
+
+        
