bbdd6aab40d50e7272517766b93de1d62e49e483
Moshe <moshe@example.com>
2026-02-26 12:25:39 +0200

    feat: create run service

--- clean-service/app/main.py
+++ clean-service/app/main.py
@@ -14,3 +14,23 @@ logger = logging.basicConfig(
     format=f'%(asctime)s | {config.service_name} | %(name)s - %(levelname)s - %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S'
 )
+
+clean_logger = logging.getLogger("clean_text")
+consumer_logger = logging.getLogger("consumer")
+producer_logger = logging.getLogger("producer")
+orchestrator_logger = logging.getLogger("orchestrator")
+
+def main():
+    try: 
+        clean_text = TextCleaner(clean_logger)
+        consumer = KafkaConsumer(config.kafka_config,config.kafka_topic_name,config.kafka_group_id,consumer_logger)
+        publisher = KafkaPublisher(producer_logger,config.kafka_config,config.kafka_topic_name)
+
+        manager = CleanOrchestrator(consumer,publisher,clean_text,orchestrator_logger)
+
+        manager.run()
+    except Exception as e:
+        raise e
+
+if __name__=="__main__":
+    main()
